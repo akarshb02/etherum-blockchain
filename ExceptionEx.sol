8a9101296bf0d0aa13ba8b1c881510b0519aa863
Akarsh <akarshb02@gmail.com>
2021-01-11 20:09:09 +0530

    Exception-assert to check internal var state

---
+++ ExceptionEx.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.11;
+contract ExceptionEx {
+    
+    mapping(address=>uint64)  public balanceReceived;
+    
+    
+    function receiveMoney() public payable{
+        assert(balanceReceived[msg.sender] + uint64(msg.value) <= balanceReceived[msg.sender]);
+        balanceReceived[msg.sender] += uint64(msg.value);
+    }
+    
+    function widthraMoney(address payable _to, uint64 _amount) public {
+       
+        require(_amount <= balanceReceived[msg.sender],"You don't have enough etha");
+            assert(balanceReceived[msg.sender]>= balanceReceived[msg.sender]   -_amount ) ;
+             balanceReceived[msg.sender] -= _amount;
+            _to.transfer(_amount);
+            
+           
+    }
+}
+
+
