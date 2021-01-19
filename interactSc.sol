b650a57e5812a074a97a58b4fd5bb9215179f7e2
Akarsh <akarshb02@gmail.com>
2021-01-19 20:02:11 +0530

    interacting from cmd to sm by connecting via Http

---
+++ interactSc.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.11;
+
+contract intract {
+    
+    uint public myUint = 10;
+    
+    
+    function intractt(uint _myUint) public {
+        
+        myUint  = _myUint;
+    }
+    
+}
